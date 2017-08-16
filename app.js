3aeb3d8ec24ec1cc36bcee90f2411f3e801724a9
Derek Egel <egelnoteagle@gmail.com>
2017-08-16 02:18:56 +0000

    finished implementing auth

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
+
 // ROUTES
 app.get("/", function(req, res){
     res.render("landing");
@@ -82,7 +87,7 @@ app.get("/campgrounds/:id", function(req, res){
 // COMMENTS ROUTES
 // ================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     // Find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -93,7 +98,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -134,6 +139,33 @@ app.post("/register", function(req, res){
         });
     });
 });
+
+// show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// handle login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+        
+    }), function(req, res) {
+});
+
+// logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 // SERVER
 app.listen(process.env.PORT, process.env.IP, function(){
