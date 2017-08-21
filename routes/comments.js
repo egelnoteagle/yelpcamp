16b62290302c4c66bfbb51bac60d248b7529815f
Derek Egel <egelnoteagle@gmail.com>
2017-08-21 23:39:35 +0000

    finished comment authorization

--- routes/comments.js
+++ routes/comments.js
@@ -42,7 +42,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 // COMMENT EDIT ROUTE
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
    Comment.findById(req.params.comment_id, function(err, foundComment){
        if(err){
            res.redirect("back");
@@ -53,8 +53,19 @@ router.get("/:comment_id/edit", function(req, res){
 });
 
 // COMMENT UPDATE ROUTE
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+      if(err){
+          res.redirect("back");
+      } else {
+          res.redirect("/campgrounds/" + req.params.id);
+      }
+   }); 
+});
+
+// COMMENTS DESTROY ROUTE
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+   Comment.findByIdAndRemove(req.params.comment_id, function(err){
       if(err){
           res.redirect("back");
       } else {
@@ -69,6 +80,25 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else {
+                // does user own comment?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
