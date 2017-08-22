8a860277c01829f2b3d36f35227cb87333a3b914
Derek Egel <egelnoteagle@gmail.com>
2017-08-22 16:27:12 +0000

    update databaseurl

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect(process.env.DATABASEURL);
 mongoose.connect("mongodb://derek:worldind@ds153123.mlab.com:53123/derek_yelp_camp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
